4145ef624184455c90ab40172eddd58a0453e392
Mohamed Romdhani <mromdhani7@gmail.com>
2018-10-17 06:53:22 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-apm-module9-frontoffice-step3-rest-node-mongodb';
+}
